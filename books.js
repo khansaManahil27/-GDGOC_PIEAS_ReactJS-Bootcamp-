68be803e33d827b370a9babe7bb1a7f95a219101
Abstract Programmer <AbstractProgrammer@example.com>
2024-11-26 19:57:06 +0500

    Initial commit

---
+++ books.js
@@ -0,0 +1,17 @@
+//  Create an array of books
+const Books = [
+    { id: 1, title: "The Catcher in the Rye", author: "J.D. Salinger", rating: 4.5, genre: "Fiction" },
+    { id: 2, title: "To Kill a Mockingbird", author: "Harper Lee", rating: 4.8, genre: "Fiction" },
+    { id: 3, title: "1984", author: "George Orwell", rating: 4.7, genre: "Fiction" },
+    { id: 4, title: "Brave New World", author: "Aldous Huxley", rating: 4.6, genre: "Science" },
+    { id: 5, title: "The Great Gatsby", author: "F. Scott Fitzgerald", rating: 4.3, genre: "Fiction" },
+    { id: 6, title: "Pride and Prejudice", author: "Jane Austen", rating: 4.9, genre: "Romance" },
+    { id: 7, title: "The Fault in Our Stars", author: "John Green", rating: 4.8, genre: "Romance" },
+    { id: 8, title: "The Time Traveler's Wife", author: "Audrey Niffenegger", rating: 4.5, genre: "Romance" },
+    { id: 9, title: "A Brief History of Time", author: "Stephen Hawking", rating: 4.7, genre: "Science" },
+    { id: 10, title: "The Theory of Everything", author: "Stephen Hawking", rating: 4.6, genre: "Science" },
+    
+];
+
+// Export the Books array
+export default Books;
