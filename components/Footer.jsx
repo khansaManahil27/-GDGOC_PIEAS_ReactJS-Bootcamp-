31218b6f2b9f92eb20fcd6c1252a5fdcb057da32
khansaManahil27 <khansamanahil306@gmail.com>
2024-12-02 11:10:44 +0500

    Delete components directory

--- components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-      <footer className="text-center py-4 bg-white border-t">
-        <p className="text-gray-600">&copy; 2024 snap. All rights reserved.</p>
-      </footer>
-    );
-  };
-  
-  export default Footer;
