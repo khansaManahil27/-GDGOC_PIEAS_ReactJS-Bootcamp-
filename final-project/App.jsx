bb36db65e6f9c1654ef539898d837c0d2339d804
khansaManahil27 <khansamanahil306@gmail.com>
2024-12-02 11:21:43 +0500

    Add files via upload

---
+++ final-project/App.jsx
@@ -0,0 +1,21 @@
+
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import Footer from " ./components/Footer";
+
+
+
+function App() {
+  return (
+    <div className="font-sans">
+      <Header />
+      <HeroSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+
+ 
